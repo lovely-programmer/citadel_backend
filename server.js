bc0b481692553a1084df055d4f745d7bff7f7706
unknown <olaoluwaolasunkanmi5@gmail.com>
2023-04-03 07:55:26 +0100

    server update

--- server.js
+++ server.js
@@ -78,11 +78,11 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
-
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
