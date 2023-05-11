17e4b663a8d5b3b20f4aaad8fe83bb6d4796f8af
unknown <olaoluwaolasunkanmi5@gmail.com>
2023-05-11 20:04:26 +0100

    update serve

--- server.js
+++ server.js
@@ -25,25 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Enable CORS
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
-
-app.use(
-  cors({
-    origin: "https://citadelchoicebank.com",
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "50mb" }));
 
